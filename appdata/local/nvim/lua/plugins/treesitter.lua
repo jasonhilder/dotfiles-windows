042329397ba96c7dab22634a984adf92f28354c8
Jason Hilder <jhilder95@gmail.com>
2024-03-11 07:20:26 +0200

    Added treesitter.

--- appdata/local/nvim/lua/plugins/treesitter.lua
+++ appdata/local/nvim/lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     lazy = true,
     build = ":TSUpdate",
+    event = "BufEnter",
     config = function()
         ---@diagnostic disable-next-line: missing-fields
         require("nvim-treesitter.configs").setup {
